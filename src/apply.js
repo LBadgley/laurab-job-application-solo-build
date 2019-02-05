6d99a4712be6ea029cb1ea32f733ecca833e2c89
Laura Badgley <l.badgleyy@gmail.com>
2019-02-04 21:51:31 -0800

    commit

--- src/apply.js
+++ src/apply.js
@@ -1,7 +1,14 @@
 const formNode = document.getElementById('form-example');
 const nameNode = document.getElementById('name');
 const locationNode = document.getElementById('location');
-const howLongRangeNode = document.getElementById('how-long');
 const availabilityTimeRangeNode = document.getElementById('availability-time');
+const strengthNumberNode = document.getElementById('strength-number');
+const yesNode = document.getElementById('yes');
+const noNode = document.getElementById('no');
 const sectionTwoNode = document.getElementById('section-two');
+const strengthRangeNode = document.getElementById('coding-strength');
 
+
+strengthRangeMode.addEventListener('change', function() {
+    strengthNumberNode.textContent =  
+});
