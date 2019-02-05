2d368a8e108c95336ac1a1ead87ea8026eb63363
Laura Badgley <l.badgleyy@gmail.com>
2019-02-04 17:46:15 -0800

    initial commit

---
+++ src/apply.js
@@ -0,0 +1,7 @@
+const formNode = document.getElementById('form-example');
+const nameNode = document.getElementById('name');
+const locationNode = document.getElementById('location');
+const howLongRangeNode = document.getElementById('how-long');
+const availabilityTimeRangeNode = document.getElementById('availability-time');
+const sectionTwoNode = document.getElementById('section-two');
+
